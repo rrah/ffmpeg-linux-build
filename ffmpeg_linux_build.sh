587ccec5680de4cf20c78446853b060b98b9d516
rrah <redsword_99@hotmail.co.uk>
2014-10-19 19:33:46 +0100

    Removed building of librtmp

--- ffmpeg_linux_build.sh
+++ ffmpeg_linux_build.sh
@@ -1,7 +1,7 @@
-# packages: libssl-dev checkinstall autoconf automake build-essential libass-dev libgpac-dev libsdl1.2-dev libtool libva-dev libvdpau-dev pkg-config texi2html zlib1g-dev git unzip
+# packages: librtmp-dev libssl-dev autoconf automake build-essential libass-dev libgpac-dev libsdl1.2-dev libtool libva-dev libvdpau-dev pkg-config texi2html zlib1g-dev git unzip
 
 check_missing_packages () {
-  local check_packages=('checkinstall' 'autoconf' 'automake' 'libtool' 'pkg-config' 'texi2html' 'git' 'unzip')
+  local check_packages=('checkinstall' 'autoconf' 'automake' 'libtool' 'pkg-config' 'texi2html' 'git' 'unzip' 'librtmp-dev')
   for package in "${check_packages[@]}"; do
     type -P "$package" >/dev/null || missing_packages=("$package" "${missing_packages[@]}")
   done
@@ -50,17 +50,6 @@ build_fdk_aac() {
 	make
 	make install
 	make distclean
-	cd /tmp/ffmpeg_source
-}
-
-
-build_rtmp() {
-	cd /tmp/ffmpeg_source
-	git clone git://git.ffmpeg.org/rtmpdump
-	cd rtmpdump
-	make
-	sudo checkinstall --pkgname=rtmpdump --pkgversion="2:$(date +%Y%m%d%H%M)-git" --backup=no \
-    --deldoc=yes --fstrans=no --default
 	cd /tmp/ffmpeg_source
 }
 
